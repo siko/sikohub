6d87a5b3666bccaee4adc06bb3eae4bdd937978a
Siko Chen <sikocb@gmail.com>
2013-10-11 16:00:12 +0800

    update of the code

--- c-study/webservice-test/add.h
+++ c-study/webservice-test/add.h
@@ -16,13 +16,12 @@
  *
  * =====================================================================================
  */
-
 //gsoap ns service name: add
 //gsoap ns service namespace: http://mail.263.net/add.wsdl
 //gsoap ns service location: http://mail.263.net
 //gsoap ns service executable: add.cgi
 //gsoap ns service encoding: encoded
 //gsoap ns schema namespace: urn:add
-int ns__add( int num1, int num2, int* sum );
 
+int ns__add( int num1, int num2, int* sum );
 
---
+++ c-study/webservice-test/addclient.cpp
@@ -0,0 +1,69 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  addclient.c
+ *
+ *    Description:  client
+ *
+ *        Version:  1.0
+ *        Created:  10/11/2013 02:54:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Siko Chen (SC), sikocb@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "soapH.h"
+#include "add.nsmap"
+
+int main(int argc, char **argv)
+{
+    int m, s; /* master and slave sockets */
+    struct soap add_soap;
+    soap_init(&add_soap);
+    //soap_set_namespaces(&add_soap); 
+    if (argc < 2)
+    {
+        printf("usage: %s <server_port> \n", argv[0]);
+        exit(1);
+    }
+    else
+    {
+        m = soap_bind(&add_soap, NULL, atoi(argv[1]), 100);
+
+        if (m < 0)
+        {
+            soap_print_fault(&add_soap, stderr);
+            exit(-1);
+        }
+
+        fprintf(stderr, "Socket connection successful: master socket = %d\n", m);
+        
+        for ( ; ; )
+        {
+            s = soap_accept(&add_soap);
+        if (s < 0)
+        {
+            soap_print_fault(&add_soap, stderr);
+            exit(-1);
+        }
+
+        fprintf(stderr, "Socket connection successful: slave socket = %d\n", s);
+        soap_serve(&add_soap);//该句说明该server的服务 
+        soap_end(&add_soap);
+        }
+    }
+    return 0;
+    }
+    
+//server端的实现函数与add.h中声明的函数相同，但是多了一个当前的soap连接的参数
+int ns__add(struct soap *add_soap, int num1, int num2, int *sum)
+{
+
+    *sum = num1 + num2;
+    return 0;
+}
+
+
