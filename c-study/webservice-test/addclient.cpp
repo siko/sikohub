223fa67bb989269628e05a763b8b058312ae765c
Siko Chen <sikocb@gmail.com>
2013-10-11 16:02:31 +0800

    update of the code

--- c-study/webservice-test/addclient.cpp
+++ c-study/webservice-test/addclient.cpp
@@ -15,55 +15,53 @@
  *
  * =====================================================================================
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include "stdsoap2.h"
 #include "soapH.h"
 #include "add.nsmap"
 
+int add(const char* server, int num1, int num2, int *sum);
+
 int main(int argc, char **argv)
 {
-    int m, s; /* master and slave sockets */
-    struct soap add_soap;
-    soap_init(&add_soap);
-    //soap_set_namespaces(&add_soap); 
-    if (argc < 2)
-    {
-        printf("usage: %s <server_port> \n", argv[0]);
-        exit(1);
-    }
-    else
-    {
-        m = soap_bind(&add_soap, NULL, atoi(argv[1]), 100);
-
-        if (m < 0)
-        {
-            soap_print_fault(&add_soap, stderr);
-            exit(-1);
-        }
-
-        fprintf(stderr, "Socket connection successful: master socket = %d\n", m);
-        
-        for ( ; ; )
-        {
-            s = soap_accept(&add_soap);
-        if (s < 0)
-        {
-            soap_print_fault(&add_soap, stderr);
-            exit(-1);
-        }
-
-        fprintf(stderr, "Socket connection successful: slave socket = %d\n", s);
-        soap_serve(&add_soap);//该句说明该server的服务 
-        soap_end(&add_soap);
-        }
-    }
-    return 0;
-    }
-    
-//server端的实现函数与add.h中声明的函数相同，但是多了一个当前的soap连接的参数
-int ns__add(struct soap *add_soap, int num1, int num2, int *sum)
-{
-
-    *sum = num1 + num2;
-    return 0;
+     int result = -1;
+     char* server="http://localhost:4567/";
+     int num1 = 0;
+     int num2 = 0;
+     int sum = 0;
+     if( argc < 3 )
+     {
+         printf("usage: %s num1 num2 \n", argv[0]);
+         exit(0);
+     }
+     num1 = atoi(argv[1]);
+     num2 = atoi(argv[2]);
+     result = add(server, num1, num2, &sum);
+     if (result != 0)
+     {
+         printf("soap err,errcode = %d\n", result);
+     }
+     else
+     {
+         printf("%d+%d=%d\n", num1, num2, sum );
+     }
+     return 0;
 }
 
+int add(const char* server, int num1, int num2, int *sum)
+{
+     struct soap add_soap;
+     int result = 0;
+     soap_init(&add_soap);
 
+     soap_call_ns__add( &add_soap, server, "", num1, num2, sum );
+     if(add_soap.error)
+     {
+         printf("soap error:%d,%s,%s\n", add_soap.error, *soap_faultcode(&add_soap), *soap_faultstring(&add_soap) );
+         result = add_soap.error;
+     }
+     soap_end(&add_soap);
+     soap_done(&add_soap);
+     return result;
+}
