c5d9c1daf3efaf43dd5f8572763709370e08af3e
Siko Chen <sikocb@gmail.com>
2020-03-23 22:19:13 +0800

    add switch

--- go-study/main.go
+++ go-study/main.go
@@ -80,6 +80,24 @@ func main() {
 		fmt.Printf("Adult")
 	}
 
+	type Gender int8
+	const (
+		MALE   Gender = 1
+		FEMALE Gender = 2
+	)
+
+	gender := MALE
+
+	switch gender {
+	case FEMALE:
+		fmt.Println("female")
+	case MALE:
+		fmt.Println("male")
+	default:
+		fmt.Println("unknown")
+	}
+	// male
+
 }
 
 func add(num int) {
