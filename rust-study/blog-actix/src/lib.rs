7f33f4a0060068e83df9a467e28d320f7cd6fc4b
Siko Chen <sikocb@gmail.com>
2022-06-06 09:40:42 +0800

    rust blog-actix:added user fun

---
+++ rust-study/blog-actix/src/lib.rs
@@ -0,0 +1,43 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+
+use actix_web::{middleware, App, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+mod errors;
+mod models;
+mod routes;
+mod schema;
+
+type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+pub struct Blog {
+    port: u16,
+}
+
+impl Blog {
+    pub fn new(port: u16) -> Self {
+        Blog { port }
+    }
+
+    pub fn run(&self, database_url: String) -> std::io::Result<()> {
+        let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+        let pool = r2d2::Pool::builder()
+            .build(manager)
+            .expect("Failed to create pool");
+
+        println!("Starting http server: 127.0.0.1:{}", self.port);
+
+        HttpServer::new(move || {
+            App::new()
+                .data(pool.clone())
+                .wrap(middleware::Logger::default())
+                .configure(routes::users::configure)
+        })
+        .bind(("127.0.0.1", self.port))?
+        .run()
+    }
+}
--- rust-study/blog-actix/src/main.rs
+++ rust-study/blog-actix/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let app = blog_actix::Blog::new(8998);
+
+    app.run(database_url)
 }
