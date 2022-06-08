037fcab1e63822e62983d7bbbbdc036f240991a0
Siko Chen <sikocb@gmail.com>
2022-06-08 09:46:56 +0800

    rust types

---
+++ rust-study/books/trpl/types/src/main.rs
@@ -0,0 +1,85 @@
+#![allow(unused)]
+fn main() {
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    //floating-point number
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder
+    let remainder = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5];
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+    println!(
+        "The value of the month at index {} is: {}",
+        index, months[index]
+    );
+}
