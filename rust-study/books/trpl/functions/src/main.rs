1b46dde7ccb7a013bad90ba17399ec0bb1340be8
Siko Chen <sikocb@gmail.com>
2022-06-08 09:51:56 +0800

    rust functions update

--- rust-study/books/trpl/functions/src/main.rs
+++ rust-study/books/trpl/functions/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    another_function(-5678);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
 }
