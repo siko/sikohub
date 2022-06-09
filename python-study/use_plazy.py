d9b27c108bf1acc79eace0bf198f11e31bf1848d
Siko Chen <sikocb@gmail.com>
2022-06-09 23:31:32 +0800

    update

--- python-study/use_plazy.py
+++ python-study/use_plazy.py
@@ -1,8 +1,11 @@
+from importlib.resources import path
 from pathlib import Path
 import plazy
+import sys
 
 if __name__ == "__main__":
-    files = plazy.list_files(root=Path.cwd(),
+    path = sys.argv[1]
+    files = plazy.list_files(root=path,
                              filter_func=lambda x: True if x.endswith(
                                  '.mdx') else False,
                              is_include_root=False)
