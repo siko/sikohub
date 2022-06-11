8e871342c0e60c3d48a570e68cf0e82db5b40048
Siko Chen <sikocb@gmail.com>
2022-06-11 21:34:55 +0800

    rust:update control flow

--- rust-study/README.md
+++ rust-study/README.md
@@ -11,3 +11,8 @@ Rust 官方文档中文翻译 https://rustwiki.org/zh-CN/
 Rust 程序设计语言 简体中文版  https://rustwiki.org/zh-CN/book/
 
 通过例子学 Rust 中文版 https://rustwiki.org/zh-CN/rust-by-example/
+
+
+tips
+----
+
--- rust-study/books/trpl/control_flow/src/main.rs
+++ rust-study/books/trpl/control_flow/src/main.rs
@@ -6,4 +6,83 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    if number != 0 {
+        println!("number was three");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
 }
