2fe70c38df181d222569944796d65f2e8606dc44
Siko Chen <sikocb@gmail.com>
2022-06-12 17:51:12 +0800

    rust:update

--- rust-study/README.md
+++ rust-study/README.md
@@ -16,3 +16,4 @@ Rust 程序设计语言 简体中文版  https://rustwiki.org/zh-CN/book/
 tips
 ----
 
+* 当变量离开作用域，Rust 为我们调用一个特殊的函数。这个函数叫做 drop，在这里 String 的作者可以放置释放内存的代码。Rust 在结尾的 } 处自动调用 drop。
--- rust-study/books/trpl/ownership/src/main.rs
+++ rust-study/books/trpl/ownership/src/main.rs
@@ -1,4 +1,10 @@
 #![allow(unused)]
 fn main() {
     let s = "hello";
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() 在字符串后追加字面值
+
+    println!("{}", s); // 将打印 `hello, world!`
 }
