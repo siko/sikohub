0e88da1b8d4c4e18e1584d96f0df2f0224a3ac44
Siko Chen <sikocb@gmail.com>
2022-06-12 19:00:38 +0800

    rust:update

--- rust-study/books/trpl/ownership/src/main.rs
+++ rust-study/books/trpl/ownership/src/main.rs
@@ -1,10 +1,62 @@
 #![allow(unused)]
 fn main() {
     let s = "hello";
-
     let mut s = String::from("hello");
 
     s.push_str(", world!"); // push_str() 在字符串后追加字面值
-
     println!("{}", s); // 将打印 `hello, world!`
+
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+    change(&mut s);
+
+    let mut s = String::from("hello world");
+    let word = first_word(&s); // word 的值为 5
+    s.clear(); // 这清空了字符串，使其等于 ""
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+}
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn first_word2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
