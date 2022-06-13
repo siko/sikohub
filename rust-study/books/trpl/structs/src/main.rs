6c92d8cee7667cf5d0b395fadda21e22ee49d807
Siko Chen <sikocb@gmail.com>
2022-06-13 20:14:53 +0800

    rust:update

--- rust-study/books/trpl/structs/src/main.rs
+++ rust-study/books/trpl/structs/src/main.rs
@@ -5,10 +5,13 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn build_user(email: String, username: String) -> User {
     User {
-        email: email,
-        username: username,
+        email,
+        username,
         active: true,
         sign_in_count: 1,
     }
@@ -19,4 +22,7 @@ fn main() {
         String::from("someone@example.com"),
         String::from("someusername123"),
     );
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
