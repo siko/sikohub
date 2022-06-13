113bab18a94376fd6cddb8c7cd3846952d5d754d
Siko Chen <sikocb@gmail.com>
2022-06-13 20:11:35 +0800

    rust:update structs

---
+++ rust-study/books/trpl/structs/src/main.rs
@@ -0,0 +1,22 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+}
