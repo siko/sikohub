ed34f9a71c349bdecc4befe41d0ee3deef95fbdf
Siko Chen <sikocb@gmail.com>
2022-06-13 20:17:51 +0800

    rust:update

--- rust-study/books/trpl/structs/src/main.rs
+++ rust-study/books/trpl/structs/src/main.rs
@@ -8,6 +8,8 @@ struct User {
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
+struct AlwaysEqual;
+
 fn build_user(email: String, username: String) -> User {
     User {
         email,
@@ -25,4 +27,6 @@ fn main() {
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
 }
