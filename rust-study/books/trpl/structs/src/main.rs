51e55daef325707a1754ea0fe52ef5f6f7788d00
Siko Chen <sikocb@gmail.com>
2022-06-13 20:36:52 +0800

    rust:debug

--- rust-study/books/trpl/structs/src/main.rs
+++ rust-study/books/trpl/structs/src/main.rs
@@ -5,6 +5,12 @@ struct User {
     sign_in_count: u64,
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
@@ -29,4 +35,21 @@ fn main() {
     let origin = Point(0, 0, 0);
 
     let subject = AlwaysEqual;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    dbg!(&rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
