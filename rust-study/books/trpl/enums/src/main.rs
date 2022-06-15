cf97b699b6b88e09a6eb1e6b0ad8916c99019651
Siko Chen <sikocb@gmail.com>
2022-06-15 22:15:14 +0800

    update

--- rust-study/books/trpl/enums/src/main.rs
+++ rust-study/books/trpl/enums/src/main.rs
@@ -1,4 +1,25 @@
 #![allow(unused)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+
+
 fn main() {
     enum IpAddrKind {
         V4,
@@ -36,19 +57,30 @@ fn main() {
     let m = Message::Write(String::from("hello"));
     m.call();
 
-    enum Option<T> {
-        None,
-        Some(T),
-    }
+    // enum Option<T> {
+    //     None,
+    //     Some(T),
+    // }
 
-    // let some_number = Some(5);
-    // let some_string = Some("a string");
+    let some_number = Some(5);
+    let some_string = Some("a string");
 
-    // let absent_number: Option<i32> = None;
+    let absent_number: Option<i32> = None;
 
-    // let x: i8 = 5;
-    // let y: Option<i8> = Some(5);
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
     // let sum = x + y;
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
 }
