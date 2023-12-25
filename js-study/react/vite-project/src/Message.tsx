c492272759b4b6fe42aa5f00acbbb50889f645aa
Siko Chen <sikocb@gmail.com>
2023-12-25 22:01:54 +0800

    page

--- js-study/react/vite-project/src/Message.tsx
+++ js-study/react/vite-project/src/Message.tsx
@@ -1,8 +1,11 @@
 
 
 function Message() {
-    return <h1>Hello World</h1>;
 
+    const name = 'Siko';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
 }
 
 export default Message;
