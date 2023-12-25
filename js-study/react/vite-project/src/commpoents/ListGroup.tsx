3a2e129ecc6d0c69f6bce5098d3f31251eca573a
Siko Chen <sikocb@gmail.com>
2023-12-25 22:59:29 +0800

    update

--- js-study/react/vite-project/src/commpoents/ListGroup.tsx
+++ js-study/react/vite-project/src/commpoents/ListGroup.tsx
@@ -1,16 +1,22 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //   items = [];
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
