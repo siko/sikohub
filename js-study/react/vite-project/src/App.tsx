12c3ae97313a7c4e4bc66c187951ec0e11c6fc95
Siko Chen <sikocb@gmail.com>
2023-12-25 23:31:10 +0800

    update

--- js-study/react/vite-project/src/App.tsx
+++ js-study/react/vite-project/src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./commpoents/ListGroup";
 function App() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const handleSeclectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} />
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSeclectItem}
+      />
     </div>
   );
 }
--- js-study/react/vite-project/src/commpoents/ListGroup.tsx
+++ js-study/react/vite-project/src/commpoents/ListGroup.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -22,6 +23,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
