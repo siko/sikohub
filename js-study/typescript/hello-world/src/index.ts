c31a8e9ec5782f7fc723ad2298102bfbb7c05102
Siko Chen <sikocb@gmail.com>
2023-12-25 12:34:00 +0800

    ts function

--- js-study/typescript/hello-world/src/index.ts
+++ js-study/typescript/hello-world/src/index.ts
@@ -53,3 +53,18 @@ function calculateTax(income: number, taxYear: number = 2022): number {
 console.log(calculateTax(100).toString());
 
 
+let employee: {
+    readonly id: number,
+    name: string,
+    retire:(date: Date) => void
+} = {
+    id: 1, 
+    name: '',
+    retire:(date:Date) => {
+        console.log(date);
+        
+    }
+};
+
+employee.name = 'hi';
+
