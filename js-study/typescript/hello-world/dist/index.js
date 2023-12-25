3a2a680e73f691e7164438e034ae0fb466af8223
Siko Chen <sikocb@gmail.com>
2023-12-25 10:05:51 +0800

    ts debug

--- js-study/typescript/hello-world/dist/index.js
+++ js-study/typescript/hello-world/dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log('Hello World');
 let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
--- js-study/typescript/hello-world/src/index.ts
+++ js-study/typescript/hello-world/src/index.ts
@@ -1,4 +1,7 @@
 console.log('Hello World');
 
 let age: number = 20;
+if (age <50)
+    age += 10;
 
+console.log(age);
