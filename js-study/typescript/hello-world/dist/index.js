c4faaf49d721913b4a77ba2cea6b363f74bf86fd
Siko Chen <sikocb@gmail.com>
2023-12-25 12:03:09 +0800

    ts type

--- js-study/typescript/hello-world/dist/index.js
+++ js-study/typescript/hello-world/dist/index.js
@@ -4,4 +4,10 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
 //# sourceMappingURL=index.js.map
--- js-study/typescript/hello-world/src/index.ts
+++ js-study/typescript/hello-world/src/index.ts
@@ -5,3 +5,18 @@ if (age <50)
     age += 10;
 
 console.log(age);
+
+
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+
+let level;
+
+level = 1;
+level = 'a';
+
+function render(document: any){
+    console.log(document);
+    
+}
