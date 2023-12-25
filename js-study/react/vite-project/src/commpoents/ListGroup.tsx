8bd03363b99c184a7c3713da2449223adb1e04a3
Siko Chen <sikocb@gmail.com>
2023-12-25 23:00:29 +0800

    update

--- js-study/react/vite-project/src/commpoents/ListGroup.tsx
+++ js-study/react/vite-project/src/commpoents/ListGroup.tsx
@@ -9,11 +9,11 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item, index) => (
+        {items.map((item) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={(event) => console.log(event)}
           >
             {item}
           </li>
