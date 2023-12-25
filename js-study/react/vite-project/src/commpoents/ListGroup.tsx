21fbbbd56dfab9de4408193c3f330caae4fc8223
Siko Chen <sikocb@gmail.com>
2023-12-25 23:16:26 +0800

    react state

--- js-study/react/vite-project/src/commpoents/ListGroup.tsx
+++ js-study/react/vite-project/src/commpoents/ListGroup.tsx
@@ -1,18 +1,27 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //   items = [];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
