036ed69730469cb24fe03d536cd65abd77d6d7a2
Siko Chen <sikocb@gmail.com>
2023-12-26 09:27:42 +0800

    react counter

--- js-study/react/learn-react/src/App.js
+++ js-study/react/learn-react/src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const name = "John";
-  const isNameShowing = true;
+  const [counter, setCounter] = useState(0);
 
-  return <div className="App">Hello {isNameShowing ? name : "someone"} !</div>;
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default App;
